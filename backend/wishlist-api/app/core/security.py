a3b3b645d2ae778666a7116e316b45b25788b10a
mvgc <mariav2601@gmail.com>
2026-01-08 10:45:54 -0400

    Update requirements and add initial config and session modules

--- backend/wishlist-api/app/core/security.py
+++ backend/wishlist-api/app/core/security.py
@@ -6,6 +6,7 @@ pwd_context = CryptContext(
 )
 
 def hash_password(password: str) -> str:
+    
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
