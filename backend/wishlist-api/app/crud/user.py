cb0f76914418895b0370bf3b1dcc9406ffad6a51
mvgc <mariav2601@gmail.com>
2026-04-09 16:11:18 -0400

    style: remove extra newline in user.py

--- backend/wishlist-api/app/crud/user.py
+++ backend/wishlist-api/app/crud/user.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from app.models.user import User
 from app.schemas.user import UserCreate
 from app.core.security import hash_password
-
+  
 def get_user_by_email(db: Session, email: str) -> User | None:
     return db.query(User).filter(User.email == email).first()
 
